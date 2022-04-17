b02cec3ee4b48b7d8c110cc8de6d1d1bf077796e
williamRiesen <51839539+williamRiesen@users.noreply.github.com>
2022-04-17 18:54:44 -0500

    Initial Commit: just displays player and rock-paper-scissors choice received

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
+radio.onReceivedValue(function (name, value) {
+    basic.showString(name)
+    if (value == 0) {
+        basic.showString("sends ROCK")
+    } else {
+        if (value == 1) {
+            basic.showString("sends PAPER")
+        } else {
+            if (value == 3) {
+                basic.showString("sends SCISSORS")
+            }
+        }
+    }
+})
 basic.forever(function () {
 	
 })
