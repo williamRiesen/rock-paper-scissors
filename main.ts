7c589e6f99fea9a95db5b8ac6047dfd8b9e6ed87
williamRiesen <51839539+williamRiesen@users.noreply.github.com>
2022-04-19 23:13:26 -0500

    Lever arm pointer works

--- main.ts
+++ main.ts
@@ -1,17 +1,68 @@
-radio.onReceivedValue(function (name, value) {
-    basic.showString(name)
-    if (value == 0) {
-        basic.showString("sends ROCK")
+function showFlag (text: string) {
+    if (text == "Player 1 Wins") {
+        pins.analogWritePin(AnalogPin.P14, 99)
+    } else if (text == "Player 2 Wins") {
+        pins.analogWritePin(AnalogPin.P14, 924)
     } else {
-        if (value == 1) {
-            basic.showString("sends PAPER")
+        pins.analogWritePin(AnalogPin.P14, 512)
+    }
+}
+function determineWinner (playerOneChoice: string, playerTwoChoice: string) {
+    if (playerOneChoice == playerTwoChoice) {
+        return "Tie Game"
+    } else if (playerOneChoice == "ROCK") {
+        if (playerTwoChoice == "SCISSORS") {
+            return "Player 1 Wins"
         } else {
-            if (value == 3) {
-                basic.showString("sends SCISSORS")
-            }
+            return "Player 2 Wins"
+        }
+    } else if (playerOneChoice == "PAPER") {
+        if (playerTwoChoice == "ROCK") {
+            return "Player 1 Wins"
+        } else {
+            return "Player 2 Wins"
+        }
+    } else {
+        if (playerTwoChoice == "PAPER") {
+            return "Player 1 Wins"
+        } else {
+            return "Player 2 Wins"
         }
     }
+}
+radio.onReceivedValue(function (name, value) {
+    if (name == "Player 1") {
+        led.plot(0, 0)
+        Player_1_Selection = toWord(value)
+        Player_1_has_gone = true
+    }
+    if (name == "Player 2") {
+        led.plot(4, 0)
+        Player_2_Selection = toWord(value)
+        Player_2_has_gone = true
+    }
+    if (Player_1_has_gone && Player_2_has_gone) {
+        showFlag(determineWinner(Player_1_Selection, Player_2_Selection))
+        Player_1_has_gone = false
+        Player_2_has_gone = false
+        basic.pause(2000)
+        radio.sendString("RESET")
+        pins.analogWritePin(AnalogPin.P14, 512)
+    }
 })
-basic.forever(function () {
-	
-})
+function toWord (num: number) {
+    if (num == 0) {
+        return "ROCK"
+    } else if (num == 1) {
+        return "PAPER"
+    } else {
+        return "SCISSORS"
+    }
+}
+let Player_2_Selection = ""
+let Player_1_Selection = ""
+let Player_2_has_gone = false
+let Player_1_has_gone = false
+radio.setGroup(1)
+Player_1_has_gone = false
+Player_2_has_gone = false
